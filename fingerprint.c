6dd76bcf4118c9b8348d00b3bd196b63eeecba3c
DancinGhost <45301064+DancinGhost@users.noreply.github.com>
2022-01-15 10:40:21 -0500

    Update and rename 6502.cpp to fingerprint.c

---
+++ fingerprint.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_AUTHOR("Julius Victorian and R");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.01");
+MODULE_DESCRIPTION("fingerprint driver");
+
+static int finger_print_init(void)
+{
+        pr_debug("Hi\n");
+        return 0;
+}
+
+static void finger_print_exit(void)
+{
+        pr_debug("Bye\n");
+}
+
+module_init(finger_print_init);
+module_exit(finger_print_exit);
