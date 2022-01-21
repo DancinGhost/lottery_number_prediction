18fcd69ea32e7d95273c3b21703d514ea6ba8c3f
DancinGhost <45301064+DancinGhost@users.noreply.github.com>
2022-01-20 23:06:54 -0500

    Update fingerprint.c

--- fingerprint.c
+++ fingerprint.c
@@ -1,22 +1,62 @@
 #include <linux/init.h>
+#include <linux/errno.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/usb.h>
 
-MODULE_AUTHOR("Julius Victorian and R");
-MODULE_LICENSE("GPL");
-MODULE_VERSION("0.01");
-MODULE_DESCRIPTION("fingerprint driver for dell xfr 6420fingerprint reader");
+#define USB_VENDOR_ID_BCM5880 0x0a5c
+#define USB_PRODUCT_ID_BCM5880 0x5801
+
+static struct usb_driver BCM5880_finger_driver;
+
+
+/* table of devices that work with this driver */
+static struct usb_device_id BCM5880_finger_driver_table [] = {
+        { USB_DEVICE(USB_VENDOR_ID_BCM5880, USB_PRODUCT_ID_BCM5880) },
+        { }                      /* Terminating entry */
+};
+MODULE_DEVICE_TABLE (usb, BCM5880_finger_driver_table);
+
+
+static int BCM5880_finger_driver_probe(struct usb_interface *interface, const struct usb_device_id *id){
+printk(KERN_INFO "FINGERPRINT DRIVER HAS LOADED CORRECTLY");
+printk(KERN_INFO "FINGERPRINT DRIVER HAS LOADED CORRECTLY");
+printk(KERN_INFO "FINGERPRINT DRIVER HAS LOADED CORRECTLY");
+printk(KERN_INFO "FINGERPRINT DRIVER HAS LOADED CORRECTLY");
+printk(KERN_INFO "FINGERPRINT DRIVER HAS LOADED CORRECTLY");
+return 0;
+}
+
 
 static int finger_print_init(void)
 {
-        pr_debug("Hi\n");
-        return 0;
+
+	int local_error_code;
+
+        /* register this driver with the USB subsystem */
+        local_error_code = usb_register(&BCM5880_finger_driver);
+        if (local_error_code < 0) {
+                printk(KERN_INFO "FAILED TO LOAD DRIVER");
+                return -1; }
+                return 0;
 }
 
 static void finger_print_exit(void)
 {
-        pr_debug("Bye\n");
+	usb_deregister(&BCM5880_finger_driver);
+
 }
+
+
+static struct usb_driver BCM5880_finger_driver = {
+	.name	=	"dell_fingerprint_driver",
+	.probe	=	BCM5880_finger_driver_probe,
+	.id_table	=	BCM5880_finger_driver_table,
+};
+
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.01");
+MODULE_DESCRIPTION("fingerprint driver");
 
 module_init(finger_print_init);
 module_exit(finger_print_exit);
